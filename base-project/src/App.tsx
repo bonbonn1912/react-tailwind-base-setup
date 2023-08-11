c2823c6f8bf0c405356bc20da45d514a93e12189
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-08-11 13:24:55 +0200

    Update App.tsx

--- base-project/src/App.tsx
+++ base-project/src/App.tsx
@@ -1,16 +1,27 @@
-
-import './index.css'
+import "./index.css";
 function App() {
   return (
-    <div className="w-screen h-screen bg-gray-400 flex justify-center items-center">
     
-      <h1 className="text-center text-5xl">
-    Hello world!
-  </h1>
+    <div className="w-screen h-screen lg:flex lg:justify-around lg:items-center">
+      <Container text={"First Container"}/>
       
+      <Container text={"Second Container"}/>
+      <Container text={"Third Container"}/>
     </div>
- 
-  )
+  );
 }
 
-export default App
+const Container = ({ text, style }: ContainerProps) => {
+  return (
+    <div className={`${style} flex items-center justify-center p-5 h-1/3 lg:h-1/4 border-blue-400 border-2 bg-blue-300 w-full lg:bg-red-200 lg:w-1/4 lg:border-2 lg:border-black lg:rounded-xl`}>
+      <h1 className="text-center text-5xl">{text}</h1>
+    </div>
+  );
+};
+
+interface ContainerProps {
+  text: string;
+  style?: string;
+}
+
+export default App;
