e29bf25fd4f6b3764141029bc2acc631ab454cfd
Dominik Wolf <58635495+bonbonn1912@users.noreply.github.com>
2023-08-11 12:48:53 +0200

    initial commit base setup

---
+++ base-project/src/App.tsx
@@ -0,0 +1,16 @@
+
+import './index.css'
+function App() {
+  return (
+    <div className="w-screen h-screen bg-gray-400 flex justify-center items-center">
+    
+      <h1 className="text-center text-5xl">
+    Hello world!
+  </h1>
+      
+    </div>
+ 
+  )
+}
+
+export default App
